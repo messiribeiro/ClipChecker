bd5f4994382355a9e6a4e2d9d3c2fedc6454c2cd
Droffyzin <59933114+messiribeiro@users.noreply.github.com>
2023-03-24 11:05:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,42 @@
 const input = document.querySelector("#clipInput");
-const inputContainer = document.querySelector(".input-container");
-
+const form = document.querySelector("#form")
+const inputContainer = document.querySelector(".form-container");
+const message = document.querySelector("#message");
 
 
 input.addEventListener("focus", () => {
-    inputContainer.classList.add("input-container-focus")
-    inputContainer.classList.remove("input-container")
+    inputContainer.classList.add("form-container-focus")
+    inputContainer.classList.remove("form-container")
 
 
 
 })
 
 input.addEventListener("blur", () => {
-    inputContainer.classList.remove("input-container-focus")
-    inputContainer.classList.add("input-container")
+    inputContainer.classList.remove("form-container-focus")
+    inputContainer.classList.add("form-container")
 
 
 })
 
+form.addEventListener("submit", (event) => {
+    if(input.value == "") {
+        alert("Bota alguma coisa ae garai")
+    }else{
+        console.log(input.value)
+        input.value = "";
+    }
 
-function getClipLink() {
-    alert(input.value)
-}
+    event.preventDefault();
+    startAnimation();
 
-input.addEventListener("submit", () => alert('eae'))
+    
+})
+ function startAnimation(){
+    message.style.display = "flex";
+    setTimeout(() => {
+        message.style.display = "none"
+    }, 985)
+ }
+
+
